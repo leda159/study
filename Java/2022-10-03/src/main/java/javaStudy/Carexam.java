438b70780467b85d7a3590a5485d014b6fedddd9
seung <106045606+leda159@users.noreply.github.com>
2022-10-03 21:37:50 +0900

    Add files via upload

---
+++ Java/2022-10-03/src/main/java/javaStudy/Carexam.java
@@ -0,0 +1,31 @@
+package javaStudy;
+
+public class Carexam {
+
+	public static void main(String[] args) {
+	
+		Car car1 = new Car();
+		System.out.println(car1.company);
+		System.out.println(car1.model);
+		System.out.println(car1.maxSpeed);
+		
+		Car car2 = new Car("자가용");
+		
+		System.out.println(car2.company);
+		System.out.println(car2.model);
+		System.out.println(car2.maxSpeed);
+		
+		Car car3 = new Car("자가용","빨강");
+		System.out.println(car3.model);
+		System.out.println(car3.color);
+		
+		Car car4 = new Car("자가용","빨강",350);
+		
+		System.out.println(car4.model);
+		System.out.println(car4.color);
+		System.out.println(car4.maxSpeed);
+
+		
+	}
+
+}
