669fcc79915a40e163b80d6b02a6fa1a507c403e
seung <106045606+leda159@users.noreply.github.com>
2022-10-03 16:09:49 +0900

    Add files via upload

---
+++ JspStudy/java2022-09-29test/bigdata/Calculator.java
@@ -0,0 +1,18 @@
+package bigdata;
+
+public class Calculator {
+
+	public int add(int num1, int num2) {
+		return num1+num2;
+	}
+	public int minus(int num1, int num2) {
+		return num1-num2;
+	}
+	public int mul(int num1, int num2) {
+		return num1*num2;
+	}
+	public double div(int num1, int num2) {
+		return (double)num1/num2;
+	}
+
+}
